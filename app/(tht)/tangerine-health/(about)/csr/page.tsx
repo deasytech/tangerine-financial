4c749ab3464bbcf502f418eda86f37b1e584e7f2
Desmond Awere <desmondawere@gmail.com>
2024-07-05 18:03:21 +0100

    local changes

--- app/(tht)/tangerine-health/(about)/csr/page.tsx
+++ app/(tht)/tangerine-health/(about)/csr/page.tsx
@@ -6,12 +6,14 @@ const CSRPage = () => {
   return (
     <>
       <section className="relative max-container padding-container flex flex-col gap-12 justify-center items-center w-full mt-44">
-        <div className="w-full space-y-3 text-center flex flex-col justify-center">
+        <div className="w-full space-y-3 text-center flex flex-col justify-center items-center">
           <h1 className="bold-48 text-blue-950">Corporate Social Responsibility</h1>
           <Image src="/images/tht/csr-banner.png" alt="CSR" width={598} height={393} className="w-full h-auto py-10" />
-          <div className="w-full bg-orange-base max-w-3xl p-10 space-y-3 rounded-3xl">
-            <p className="regular-18 text-white !leading-[145%]">We are committed to making a positive impact on the health and well-being of our community. As a responsible corporate citizen, THT understands the importance of Corporate Social Responsibility (CSR) in contributing to a healthier, happier and sustainable society.</p>
-            <p className="regular-18 text-white !leading-[145%]">Our CSR initiatives are designed to address critical health-related issues, promote wellness, and create a lasting positive impact on the communities we serve through advocacy, education and events sponsorship.</p>
+          <div className="w-full flex justify-center">
+            <div className="bg-orange-base max-w-3xl p-10 space-y-3 rounded-3xl">
+              <p className="regular-18 text-white !leading-[145%]">We are committed to making a positive impact on the health and well-being of our community. As a responsible corporate citizen, THT understands the importance of Corporate Social Responsibility (CSR) in contributing to a healthier, happier and sustainable society.</p>
+              <p className="regular-18 text-white !leading-[145%]">Our CSR initiatives are designed to address critical health-related issues, promote wellness, and create a lasting positive impact on the communities we serve through advocacy, education and events sponsorship.</p>
+            </div>
           </div>
         </div>
       </section>
