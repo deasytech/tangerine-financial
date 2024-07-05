c7455f242e3e7fd1cc0ad0e6551c81c39bdfebf4
Desmond Awere <desmondawere@gmail.com>
2024-07-05 16:05:08 +0100

    made corrections of tangerine web

--- components/cards/SubsidaryCard.tsx
+++ components/cards/SubsidaryCard.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
-import Button from "../Button";
-import Image from "next/image";
 import { ArrowRight } from "lucide-react";
+import { Button } from "../ui/button";
 
 interface Props {
   title: string;
@@ -16,7 +15,7 @@ interface Props {
 
 const SubsidaryCard = ({ title, titleStyle, subTitle, subTitleStyle, description, link, linkStyle, bgStyle }: Props) => {
   return (
-    <div className={`w-full px-5 py-8 rounded-3xl flex-col justify-between flex gap-4 ${bgStyle}`}>
+    <div className={`w-full px-5 py-8 rounded-xl flex-col justify-between flex gap-4 ${bgStyle}`}>
       <div className="flex-col justify-start items-start gap-4 flex">
         <div className="flex-col justify-start items-start gap-2 flex">
           <div className={`regular-12 uppercase ${subTitleStyle}`}>{subTitle}</div>
@@ -25,8 +24,10 @@ const SubsidaryCard = ({ title, titleStyle, subTitle, subTitleStyle, description
         <div className="regular-12">{description}</div>
       </div>
       <div className="flex justify-end mt-10">
-        <Link href={link} className={`p-2 rounded-lg ${linkStyle}`}>
-          <ArrowRight color="white" size={16} />
+        <Link href={link}>
+          <Button size="icon" className={linkStyle}>
+            <ArrowRight size={18} />
+          </Button>
         </Link>
       </div>
     </div>
